aa11a75d07f401f9ee88233a2242cd7efbeef069
Karthikraj <karthikrajrpa@gmail.com>
2024-04-14 21:01:35 +0530

    update for submit

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World'
 
+@app.route('/submit', actions=["POST","GET"])
+def hello_world_submit():
+    return 'Hello World Submit'
+
 if __name__ == '__main__':
     app.run()
